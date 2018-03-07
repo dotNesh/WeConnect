4845d5da3fca5996f4ac3179f4b28ee0a7853f68
Kariuki Kelvin <kmunene@live.com>
2018-03-07 21:27:11 +0300

    [Feature #155787722] registration endpoint tests

--- tests/test_weconnect.py
+++ tests/test_weconnect.py
@@ -111,6 +111,51 @@ class UserendpointsTestcase(unittest.TestCase):
         self.assertEqual(response.status_code, 404)
         response_msg = json.loads(response.data.decode("UTF-8"))
         self.assertEqual(response_msg["message"],"Non-existent user. Try signing up") 
+        
+class BusinessendpointsTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client(self)
+
+        self.app.post("/api/v1/auth/register",
+                    data=json.dumps(dict(email="kelvin@live",username="kelvin",
+                                password="12345678")), content_type="application/json")
+
+        
+        self.login_user = self.app.post("/api/v1/auth/login",
+                        data=json.dumps(dict(username="kelvin",password="12345678")),
+                                         content_type="application/json") 
+      
+
+    def test_add_business(self):
+        access_token = json.loads(self.login_user.data.decode())['access_token']
+
+        response = self.app.post("/api/v1/businesses",
+                                data=json.dumps(dict(
+                                    business_name="Andela",
+                                    category="software",
+                                    location="Nairobi",
+                                    description="This is Andela")
+                                ),
+                                headers = {
+                                    "Authorization": "Bearer {}".format(access_token),
+                                    "Content-Type": "application/json"
+                                })
+                       
+
+
+        self.assertEqual(response.status_code, 201)
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertEqual(response_msg["message"],"Business Successfully Registered") 
+
+    def test_get_business(self):
+         access_token = json.loads(self.login_user.data.decode())['access_token']
+
+         response = self.app.get("/api/v1/businesses",
+                                    headers = {
+                                    "Authorization": "Bearer {}".format(access_token),
+                                    "Content-Type": "application/json"
+                                    })
+         self.assertEqual(response.status_code, 200)        
 
 if __name__ == '__main__':
     unittest.main()        
