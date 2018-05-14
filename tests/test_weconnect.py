cda1cd86c32e0336a3f533d13f6aca2d387e9fd6
Kelvin Kariuki <kelvin.kariuki@andela.com>
2018-05-14 16:36:39 +0300

    added tests for reset endpoint and length validation

--- tests/test_weconnect.py
+++ tests/test_weconnect.py
@@ -7,7 +7,7 @@ class UserendpointsTestcase(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client(self)
         self.app.post("/api/v1/auth/register",
-                      data=json.dumps(dict(email="kelvin@live", username="kelvin",
+                      data=json.dumps(dict(email="kmunene@live.com", username="kelvin",
                                            password="12345678")), content_type="application/json")
 
         self.app.post("/api/v1/auth/register",
@@ -40,6 +40,17 @@ class UserendpointsTestcase(unittest.TestCase):
         self.assertEqual(response.status_code, 201)
         response_msg = json.loads(response.data.decode("UTF-8"))
         self.assertEqual(response_msg["message"], "User Succesfully Registered")
+    
+    def test_password_length(self):
+        '''Test password length'''
+        response = self.app.post("/api/v1/auth/register",
+                                 data=json.dumps(dict(email="mutha@live", username="mutha",
+                                                      password="123456")),
+                                 content_type="application/json")
+
+        self.assertEqual(response.status_code, 406)
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertEqual(response_msg["message"], "Password is weak! Must have atleast 8 characters")
 
     def test_email_not_empty(self):
         '''Test for blank email input'''
@@ -126,7 +137,7 @@ class UserendpointsTestcase(unittest.TestCase):
     def test_email_already_registered(self):
         '''Test for existing email'''
         response = self.app.post("/api/v1/auth/register",
-                                 data=json.dumps(dict(email="kelvin@live", username="kelin",
+                                 data=json.dumps(dict(email="dee@live", username="kelin",
                                                       password="12345678")),
                                  content_type="application/json")
 
@@ -217,9 +228,9 @@ class UserendpointsTestcase(unittest.TestCase):
         self.assertEqual(response_msg["message"], "Non-existent user. Try signing up")
 
 
-    def test_reset_missing_old_password(self):
+    def test_change_missing_old_password(self):
         '''Test for missing old password'''
-        response = self.app.post("/api/v1/auth/reset-password",
+        response = self.app.post("/api/v1/auth/change-password",
                                  data=json.dumps(dict(new_password="12345667")),
                                  headers={
                                      "Authorization": "Bearer {}".format(self.access_token),
@@ -231,9 +242,9 @@ class UserendpointsTestcase(unittest.TestCase):
         self.assertEqual(response_msg['old_password-Error:']["message"],
                          "old_password cannot be missing")
 
-    def test_reset_missing_new_password(self):
+    def test_change_missing_new_password(self):
         '''Test for missing new password'''
-        response = self.app.post("/api/v1/auth/reset-password",
+        response = self.app.post("/api/v1/auth/change-password",
                                  data=json.dumps(dict(old_password="12345667")),
                                  headers={
                                      "Authorization": "Bearer {}".format(self.access_token),
@@ -244,13 +255,13 @@ class UserendpointsTestcase(unittest.TestCase):
         response_msg = json.loads(response.data.decode("UTF-8"))
         self.assertEqual(response_msg['new_password-Error:']["message"],
                          "new_password cannot be missing")
-    def test_password_reset(self):
-        '''Test for password reset'''
+    def test_password_change(self):
+        '''Test for password change'''
         access_token = json.loads(self.user.data.decode())['access_token']
 
-        response = self.app.post("/api/v1/auth/reset-password",
+        response = self.app.post("/api/v1/auth/change-password",
                                  data=json.dumps(dict(old_password="12345678",
-                                                      new_password="12345")),
+                                                      new_password="12348765")),
                                  headers={
                                      "Authorization": "Bearer {}".format(access_token),
                                      "Content-Type": "application/json"
@@ -258,35 +269,56 @@ class UserendpointsTestcase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         response_msg = json.loads(response.data.decode("UTF-8"))
         self.assertEqual(response_msg["message"], "Reset successful")
-    def test_login_after_password_reset(self):
-        '''Test for login after reset'''
+
+    def test_password_change_length(self):
+        '''Test for password length in password'''
+        user = self.app.post("/api/v1/auth/login",
+                                 data=json.dumps(dict(username="dee", password="12348765")),
+                                 content_type="application/json")
+
+        access_token = json.loads(user.data.decode())['access_token']
+
+        response = self.app.post("/api/v1/auth/change-password",
+                                 data=json.dumps(dict(old_password="12348765",
+                                                      new_password="123487")),
+                                 headers={
+                                     "Authorization": "Bearer {}".format(access_token),
+                                     "Content-Type": "application/json"
+                                     })
+        self.assertEqual(response.status_code, 406)
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertEqual(response_msg["message"], "Password is weak! Must have atleast 8 characters")
+    
+    
+    def test_login_after_password_change(self):
+        '''Test for login after change'''
         access_token = json.loads(self.user2.data.decode())['access_token']
 
-        self.app.post("/api/v1/auth/reset-password",
+        self.app.post("/api/v1/auth/change-password",
                       data=json.dumps(dict(old_password="12345678",
-                                           new_password="12345")),
+                                           new_password="12348765")),
                       headers={
                           "Authorization": "Bearer {}".format(access_token),
                           "Content-Type": "application/json"
                           })
 
         response = self.app.post("/api/v1/auth/login",
-                                 data=json.dumps(dict(username="dee", password="12345")),
+                                 data=json.dumps(dict(username="dee", password="12348765")),
                                  content_type="application/json")
         self.assertEqual(response.status_code, 200)
         response_msg = json.loads(response.data.decode("UTF-8"))
         self.assertEqual(response_msg["message"], "You are logged in successfully")
-    def test_wrong_password_reset(self):
-        '''Test for wrong old password in reset'''
+    def test_wrong_password_change(self):
+        '''Test for wrong old password in change'''
         user = self.app.post("/api/v1/auth/login",
-                                 data=json.dumps(dict(username="ed", password="12345")),
+                                 data=json.dumps(dict(username="ed", password="12348765")),
                                  content_type="application/json")
 
         access_token = json.loads(user.data.decode())['access_token']
 
-        response = self.app.post("/api/v1/auth/reset-password",
+        response = self.app.post("/api/v1/auth/change-password",
                                  data=json.dumps(dict(old_password="12345667",
-                                                      new_password="12345!@")),
+                                                      new_password="12345!@0")),
                                  headers={
                                      "Authorization": "Bearer {}".format(access_token),
                                      "Content-Type": "application/json"
@@ -294,7 +326,34 @@ class UserendpointsTestcase(unittest.TestCase):
         self.assertEqual(response.status_code, 401)
         response_msg = json.loads(response.data.decode("UTF-8"))
         self.assertEqual(response_msg["message"],
-                         "Wrong Password. Cannot reset. Forgotten password?")                    
+                         "Wrong Password. Cannot reset. Forgotten password?")    
+
+    def test_password_reset(self):
+        '''Test for password reset'''
+        response = self.app.post("/api/v1/auth/reset-password",
+                                  data=json.dumps(dict(username="dee")),
+                                  headers ={
+                                      "Content-Type": "application/json"   
+                                  })
+        self.assertEqual(response.status_code, 200)
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertEqual(response_msg["message"],
+                         "An email has been sent with your new password!")
+    
+    def test_password_reset_no_user(self):
+        '''Test for reset if no existing user'''
+        response = self.app.post("/api/v1/auth/reset-password",
+                                  data=json.dumps(dict(username="deelo")),
+                                  headers ={
+                                      "Content-Type": "application/json"   
+                                  })
+        self.assertEqual(response.status_code, 404)
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertEqual(response_msg["message"],
+                         "Non-existent user. Try signing up")
+        
+
+
 
 if __name__ == '__main__':
     unittest.main()
